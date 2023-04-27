dfba2a1d17b9e26f0406e6e95c2eee774f87d23e
ehsanm <ehsanmohamadi780@gmail.com>
2023-04-27 15:57:16 +0430

    add initial files of project

---
+++ my-project/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { Header } from './layout/Harder/header';
+import { Main } from './layout/Main/main';
+import { Footer } from './layout/Footer/footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ my-project/src/layout/Harder/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <>
+      <div className="bg-header text-white p-2">React Shopping Cart</div>
+    </>
+  );
+};
