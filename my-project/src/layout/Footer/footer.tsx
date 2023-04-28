367b2196af92e1110759211fbc06aa78c2860754
ehsanm <ehsanmohamadi780@gmail.com>
2023-04-28 10:19:17 +0430

    add footer

--- my-project/src/layout/Footer/footer.tsx
+++ my-project/src/layout/Footer/footer.tsx
@@ -1,3 +1,9 @@
 export const Footer = () => {
-  return <></>;
+  return (
+    <>
+      <div className="bg-header text-white p-2 flex justify-center">
+        All right is reserved
+      </div>
+    </>
+  );
 };
